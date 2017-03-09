09493ff9c8cebfc1af7ed92b1b12cda0ec98928f
Alan.Chang <mobilesleepz@gmail.com>
2017-03-09 22:23:49 +0800

    seeds、faker、 Model相關

---
+++ app/Models/Post.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model {
+
+    protected $fillable = [
+        'title',
+        'sub_title',
+        'content',
+        'is_feature',
+        'page_view',
+    ];
+
+    public function comments(){
+        return $this->hasMany('\App\Models\Comment');
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,23 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
+
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
+    public function run() {
+        Model::unguard();
+
         // $this->call(UsersTableSeeder::class);
+        $this->call(PostsTableSeeder::class);
+        $this->call(CommentsTableSeeder::class);
+
+        Model::reguard();
     }
+
 }
